b5256e860ce8aa7b743e9992cc16886760dbcb66
Caerwent <caerwent@gmail.com>
2021-12-31 15:56:55 +0100

    update linux scripts

--- atalierou/make_dist.sh
+++ atalierou/make_dist.sh
@@ -3,12 +3,18 @@
 # https://digitalboxweb.wordpress.com/2021/01/20/distribuer-son-editeur-de-texte-multi-plateformes/
 
 DISTRIB_PATH=../distrib/linux_x86-64
-BUILD_PATH=../build-Atalierou-Qt_6_2_2_gcc_64
-rmdir -rf $DISTRIB_PATH
-mkdirs $DISTRIB_PATH
-mkdirs $DISTRIB_PATH/plugins/platforms
-cp $BUILD_PATH/Atalierou .$DISTRIB_PATH
+BUILD_PATH=../build-Atalierou-Desktop_Qt_6_2_2_GCC_64bit-Release
+LIBS_PATH=$DISTRIB_PATH/libs
+rm -rf $DISTRIB_PATH
+mkdir -p $DISTRIB_PATH
+mkdir -p $LIBS_PATH
+mkdir -p $DISTRIB_PATH/plugins/platforms
+cp $BUILD_PATH/Atalierou $DISTRIB_PATH
 cp ./icon.png $DISTRIB_PATH
 cp ./desktop.entry $DISTRIB_PATH
 cp ./Atalierou.sh $DISTRIB_PATH
 cp ./qt.conf $DISTRIB_PATH
+
+ldd $BUILD_PATH/Atalierou  | column -t -d -H 1,2,4 -s " " | grep Qt | xargs cp -t $LIBS_PATH
+ldd $BUILD_PATH/Atalierou  | column -t -d -H 1,2,4 -s " " | grep xcb.so | xargs cp -t $DISTRIB_PATH/plugins/platforms
+
