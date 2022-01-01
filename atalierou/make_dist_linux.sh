9028593f07ad22d81eb4f0db87e11c9452e034d7
Caerwent <caerwent@gmail.com>
2022-01-01 15:15:55 +0100

    start windows deploy script

--- atalierou/make_dist_linux.sh
+++ atalierou/make_dist_linux.sh
@@ -4,7 +4,14 @@
 
 QT_PATH=../../Qt/6.2.2/gcc_64/bin
 BUILD_PATH=../build-Atalierou-Desktop_Qt_6_2_2_GCC_64bit-Release
+DISTRIB_PATH=../distrib/linux_x86-64
+rm -rf $DISTRIB_PATH
+mkdir -p $DISTRIB_PATH
+cp $BUILD_PATH/Atalierou $DISTRIB_PATH
+cp ./icon.png $DISTRIB_PATH
+cp ./Atalierou.desktop $DISTRIB_PATH
 
-../../linuxdeployqt-continuous-x86_64.AppImage -qmake=$QT_PATH -appimage -verbose=3 Atalierou.desktop
+cd $DISTRIB_PATH
+../../../../linuxdeployqt-continuous-x86_64.AppImage -qmake=$QT_PATH -appimage -verbose=3 Atalierou.desktop
 
 
