256d7c8851d38335ed319f5675aa0934fcf7ee13
Caerwent <caerwent@gmail.com>
2022-01-19 17:04:37 +0100

    build: update build workflow for windows

--- make_windows.ps1
+++ make_windows.ps1
@@ -1,61 +1,49 @@
 
-if not defined QT_DIR (
-  echo QT_DIR is NOT defined, used default value
-  set QT_DIR=../Qt/6.2.2/gcc_64
-)
-
-::// find absolute path, save current directory and change to target directory
-pushd %QT_DIR%
-:: // Save value of CD variable (current directory)
-set QT_DIR=%CD%
-:: // Restore original directory
-popd
-
-echo QT_DIR: %QT_DIR%
+if ($null -eq $env:QT_DIR) {
+    echo QT_DIR is NOT defined, used default value
+    
+    $env:QT_DIR = ../Qt/6.2.2/gcc_64 
+}
+$env:QT_DIR=[System.IO.Path]::GetFullPath($env:QT_DIR)
+echo QT_DIR $Env:QT_DIR
 
 
-if not defined CURRENT_VERSION (
-  echo CURRENT_VERSION is NOT defined, used default value
-  set CURRENT_VERSION=1.0.0
-)
-
-echo CURRENT_VERSION: %CURRENT_VERSION%
-
-if not defined ARCH_NAME (
-  echo ARCH_NAME is NOT defined, used default value
-  set ARCH_NAME=x86_64
-)
-
-  
-set ROOT_PATH=%CD%
-set PROJECT_PATH=./atalierou
-pushd %PROJECT_PATH%
-set PROJECT_PATH=%CD%
-popd
-
-set BUILD_PATH=./build-Atalierou-windows_$ARCH_NAME-Release
-RMDIR /S /Q %BUILD_PATH%
-mkdir  %BUILD_PATH%
-pushd %BUILD_PATH%
-set BUILD_PATH=%CD%
-popd
-
-set DISTRIB_PATH=./distrib/windows_$ARCH_NAME
-RMDIR /S /Q %DISTRIB_PATH%
-mkdir %DISTRIB_PATH%
-pushd %DISTRIB_PATH%
-set DISTRIB_PATH=%CD%
-popd
-
-cd %BUILD_PATH%
+if ($null -eq $env:CURRENT_VERSION) {
+    echo CURRENT_VERSION is NOT defined, used default value
+    
+    $env:CURRENT_VERSION=1.0.0
+}
 
 
-%QT_DIR%/bin/qmake -o Makefile %PROJECT_PATH%/Atalierou.pro -spec win32-msvc CONFIG+=qtquickcompiler
+echo CURRENT_VERSION $env:CURRENT_VERSION
 
-%QT_DIR%/bin/nmake -f Makefile
+if ($null -eq $env:) {
+    echo ARCH_NAME is NOT defined, used default value
+    
+    $env:ARCH_NAME=x86_64
+}
 
 
-%QT_DIR%/bin/windeployqt --release --qmldir --qmake %QT_DIR%/bin/qmake %PROJECT_PATH%/qml --verbose 2 Atalierou.exe 
-cd %ROOT_PATH%
-mv %BUILD_PATH%/Atalierou.exe Atalierou_${CURRENT_VERSION}_${ARCH_NAME}.exe
+
+set $env:PROJECT_PATH=[System.IO.Path]::GetFullPath("./atalierou")
+
+
+set $env:BUILD_PATH=[System.IO.Path]::GetFullPath("./build-Atalierou-windows_$env:ARCH_NAME-Release")
+New-Item -Force -Path $env:BUILD_PATH -ItemType Directory
+
+set $env:DISTRIB_PATH=[System.IO.Path]::GetFullPath("./distrib/windows_$env:ARCH_NAME")
+New-Item -Force -Path $env:DISTRIB_PATH -ItemType Directory
+
+Set-Location -Path $env:BUILD_PATH -PassThru
+
+
+$env:QT_DIR/bin/qmake -o Makefile %PROJECT_PATH%/Atalierou.pro -spec win32-msvc CONFIG+=qtquickcompiler
+
+$env:QT_DIR/bin/nmake -f Makefile
+
+
+$env:QT_DIR/bin/windeployqt --release --qmldir --qmake $env:QT_DIR/bin/qmake $env:PROJECT_PATH/qml --verbose 2 Atalierou.exe 
+
+Set-Location -Path $env:ROOT_PATH -PassThru
+mv $env:BUILD_PATH/Atalierou.exe Atalierou_$env:CURRENT_VERSION_$env:ARCH_NAME.exe
 
