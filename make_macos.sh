a37f18c1b7d226f2004922edeaeb27602b566996
Vincent Tézé <vincent.teze@orange.com>
2024-08-30 10:52:43 +0200

    update env variables

--- make_macos.sh
+++ make_macos.sh
@@ -4,7 +4,7 @@ brew list coreutils || brew install coreutils
 
 if [ -z "${QT_DIR}" ]; then
   echo "No QT_DIR defined, used default value"
-  export QT_DIR=$(realpath ../Qt/6.2.4/gcc_64)
+  export QT_DIR=$(realpath ../../Tools/6.7.2/macos)
 fi
 echo "QT_DIR: $QT_DIR"
 
